e3db2408d8bd71b4cf8c071525dbcc6bd50d6518
Macsaroo <sajjad.sarookhani@gmail.com>
2025-04-04 00:38:15 +0330

    add age property to human class

--- src/example/Human.java
+++ src/example/Human.java
@@ -1,19 +1,21 @@
 package example;
+
 import db.*;
 
 
 public class Human extends Entity {
     public String name;
+    public int age;
 
-    public Human(String name) {
+    public Human(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     @Override
     public Human copy() {
-        Human copyHuman = new Human(name);
+        Human copyHuman = new Human(name , age);
         copyHuman.id = id;
-
         return copyHuman;
     }
 }
