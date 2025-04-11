7799114a53ced2c6eed7eb41279e3fa45d15d632
Macsaroo <sajjad.sarookhani@gmail.com>
2025-04-11 15:57:34 +0330

    add task validator class and write their methods and fields

---
+++ src/todo/validator/TaskValidator.java
@@ -0,0 +1,18 @@
+package todo.validator;
+
+import db.*;
+import db.exception.InvalidEntityException;
+import todo.entity.Task;
+
+public class TaskValidator implements Validator {
+    public void validate(Entity entity) throws InvalidEntityException {
+        if (!(entity instanceof Task task))
+            throw new IllegalArgumentException("Invalid entity type. Expected Task.");
+
+
+        if (task.title == null || task.title.isEmpty())
+            throw new InvalidEntityException("Task title cannot be empty.");
+
+    }
+
+}
