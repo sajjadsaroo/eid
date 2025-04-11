40246b7fefadd5f844377a7129e8effb425ffb16
sajjadsaroo <78489169+sajjadsaroo@users.noreply.github.com>
2025-04-11 20:08:15 +0330

    Update StepSerializer.java

--- src/todo/serializer/StepSerializer.java
+++ src/todo/serializer/StepSerializer.java
@@ -1,5 +1,7 @@
 package todo.serializer;
-import db.*;
+
+import db.Serializer;
+import db.Entity;
 import todo.entity.Step;
 
 public class StepSerializer implements Serializer {
@@ -7,18 +9,26 @@ public class StepSerializer implements Serializer {
     @Override
     public String serialize(Entity e) {
         Step step = (Step) e;
-        return step.id + "|" + step.title + "|" + step.status.name() + "|" + step.taskRef;
+        return step.getEntityCode() + "|" + step.id + "|" + step.title + "|" + step.status.name() + "|" + step.taskRef;
     }
 
     @Override
     public Entity deserialize(String s) {
         String[] parts = s.split("\\|");
-        if (parts.length != 4) throw new IllegalArgumentException("Invalid Step data format");
+        if (parts.length != 5) {
+            throw new IllegalArgumentException("Invalid Step data format");
+        }
 
-        Step step = new Step(parts[1], Integer.parseInt(parts[3]));
-        step.id = Integer.parseInt(parts[0]);
-        step.status = Step.Status.valueOf(parts[2]);
+        try {
+            int entityCode = Integer.parseInt(parts[0]);
+            Step step = new Step(parts[2], Integer.parseInt(parts[4]));
+            step.id = Integer.parseInt(parts[1]);
+            step.status = Step.Status.valueOf(parts[3]);
 
-        return step;
+            return step;
+        } catch (Exception e) {
+            System.out.println("Error deserializing Step: " + e.getMessage());
+            return null;
+        }
     }
 }
